0f6874dc90aa5fab0395c4e7a1d8152d3739c026
William Griffiths <willjgriff@gmail.com>
2020-10-07 17:39:03 +0100

    Update to use calling address as address to verify.

--- scripts/create-verification.js
+++ scripts/create-verification.js
@@ -1,37 +1,35 @@
-// Update ADDRESS_TO_VERIFY and run with 'npx buidler run scripts/create-verification.js --network rinkeby'
+// Run with 'npx buidler run scripts/create-verification.js --network rinkeby'
+// Requires the private key from the key you want to verify be in process.env.ETH_KEY as specified in buidler.config.js
 
 const BrightIdRegister = artifacts.require('BrightIdRegister')
 const ethers = require('ethers')
 
-const ADDRESS_TO_VERIFY = '0xdf456B614fE9FF1C7c0B380330Da29C96d40FB02'
-
-const BRIGHTID_REGISTER_ADDRESS = '0xe13117651ebd5ec14e190b049eff7366f685c71a'
+const BRIGHTID_REGISTER_ADDRESS = '0x001797759797edcdce090b391362a1012c50a06f'
 const VERIFICATIONS_PRIVATE_KEY = '0xd49743deccbccc5dc7baa8e69e5be03298da8688a15dd202e20f15d5e0e9a9fb' // Public address 0xead9c93b79ae7c1591b1fb5323bd777e86e150d4
 const BRIGHT_ID_CONTEXT = '0x3168697665000000000000000000000000000000000000000000000000000000' // stringToBytes32("1hive")
-const CONTEXT_IDS = [ADDRESS_TO_VERIFY]
 const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
-const getVerificationsSignature = async (timestamp) => {
+const getVerificationsSignature = async (timestamp, contextIds) => {
   const hashedMessage = web3.utils.soliditySha3(
     BRIGHT_ID_CONTEXT,
-    { type: 'address[]', value: CONTEXT_IDS },
+    { type: 'address[]', value: contextIds },
     timestamp
   )
   const signingKey = new ethers.utils.SigningKey(VERIFICATIONS_PRIVATE_KEY)
-  const signDigest = signingKey.signDigest(hashedMessage)
-  console.log(signDigest)
-
-  return signDigest
+  return signingKey.signDigest(hashedMessage)
 }
 
 const verifyWithBrightIdRegister = async () => {
   const timestamp = (await web3.eth.getBlock('latest')).timestamp
   const brightIdRegister = await BrightIdRegister.at(BRIGHTID_REGISTER_ADDRESS)
-  const signature = await getVerificationsSignature(timestamp)
+  const addressToVerify = (await web3.eth.getAccounts())[0]
+  const contextIds = [addressToVerify]
 
-  console.log(`Registering ${ADDRESS_TO_VERIFY}...`)
-  await brightIdRegister.register(BRIGHT_ID_CONTEXT, CONTEXT_IDS, timestamp, signature.v, signature.r, signature.s, ZERO_ADDRESS, '0x0')
-  console.log('Is verified: ', await brightIdRegister.isVerified(ADDRESS_TO_VERIFY))
+  const signature = await getVerificationsSignature(timestamp, contextIds)
+
+  console.log(`Registering ${addressToVerify}...`)
+  await brightIdRegister.register(BRIGHT_ID_CONTEXT, contextIds, timestamp, signature.v, signature.r, signature.s, ZERO_ADDRESS, '0x0')
+  console.log('Is verified: ', await brightIdRegister.isVerified(addressToVerify))
 }
 
 verifyWithBrightIdRegister()
